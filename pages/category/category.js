56ae0503955ca2d9eb84cbc117c3c8a36d3fd846
huoerica <huojinli107@126.com>
2019-04-10 17:55:48 +0800

    swiper禁止手动切换

--- pages/category/category.js
+++ pages/category/category.js
@@ -8,12 +8,10 @@ Page({
   data: {
     toView: 'red',
     scrollTop: 100,
-    category: [
-      {
+    category: [{
         catename: '果味',
         banner: '/lib/img/pic/4.jpg',
-        list: [
-          {
+        list: [{
             catename: '梨花带雨',
             number: 5,
             picture: '/lib/img/pic/9.jpg',
@@ -66,8 +64,7 @@ Page({
       {
         catename: '蔬菜',
         banner: '/lib/img/pic/4.jpg',
-        list: [
-          {
+        list: [{
             catename: '芹菜',
             number: 5,
             picture: '/lib/img/pic/9.jpg',
@@ -102,8 +99,7 @@ Page({
       {
         catename: '炒货',
         banner: '/lib/img/pic/4.jpg',
-        list: [
-          {
+        list: [{
             catename: '油炸花生',
             number: 5,
             picture: '/lib/img/pic/9.jpg',
@@ -138,8 +134,7 @@ Page({
       {
         catename: '点心',
         banner: '/lib/img/pic/4.jpg',
-        list: [
-          {
+        list: [{
             catename: '小红的猪耳朵',
             number: 5,
             picture: '/lib/img/pic/9.jpg',
@@ -168,8 +163,7 @@ Page({
       {
         catename: '粗茶',
         banner: '/lib/img/pic/4.jpg',
-        list: [
-          {
+        list: [{
             catename: '红柚枸杞',
             number: 5,
             picture: '/lib/img/pic/9.jpg',
@@ -205,72 +199,68 @@ Page({
     currcate: 0,
     autoplay: false
   },
-  changeCate: function (e) {
+  changeCate: function(e) {
     console.log(e.target.dataset.index);
     this.setData({
-      currcate: e.target.dataset.index,
-      autoplay: true
+      currcate: e.target.dataset.index
     });
-    setTimeout(()=>{
-      this.setData({
-        autoplay: false
-      });
-    },500);
   },
-  
+  catchTouchMove (event) {
+    return false;
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
